4b01806db7455e831f62b08ded4a731b123c2214
hammad-sohail <hammadsohail733@gmail.com>
2025-08-05 14:07:16 +0500

    Q7

---
+++ app.js
@@ -0,0 +1,60 @@
+// Q1
+
+
+// let arr = ["apple", "banana", "cherry"]
+// for(let i = 0; i < arr.length;i++){
+//     console.log(arr [i]);
+// }
+
+// Q2
+
+// let para = document.querySelector("#para");
+// let arrColors = ["red", "green", "blue"];
+// for(let i = 0 ; i < arrColors.length; i++){
+//     para.innerHTML += "<p>" + arrColors [i] + "</p>";
+// }
+
+// Q3
+
+// for(let i = 0 ; i <=10; i++){
+//     console.log(i);
+// }
+
+// Q4
+
+// let i = 0;
+// while(i <=5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// Q5
+// let div = document.querySelector("#div");
+// let nameArr = ["Ali", "Sara", "Ahmed"];
+// for(let i = 0 ; i <nameArr.length;i++){
+//     div.innerHTML += "<span>" +  " , "  + nameArr[i] + "</span>"
+// }
+
+// Q6
+
+
+
+// let para =document.querySelector("#status");
+// para.innerHTML = "Changed";
+// let sumArr = [5, 10, 15]
+// let sumNum = 0;
+// let sum = document.querySelector("#sum");
+// for(let i = 0 ; i <sumArr.length; i++){
+//     sum.innerHTML = sumNum += sumArr[i];
+// }
+
+
+// Q7
+let para = document.querySelector("#citeis");
+let cities =["Karachi", "Lahore", "Quetta"]
+let newArr = cities.split (" , ");
+console.log(newArr);
+for(let i = 0 ; i <newArrCities.lenght; i++){
+    para.innerHTML = newArrCities [i];
+}
